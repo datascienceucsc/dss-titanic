2c1364451b0dfa1597b6d8cd361f74d484927474
Unknown <1624udon@gmail.com>
2019-02-21 19:04:40 -0800

    Created function for data preprocessing
    Still unable to import impute from sklearn

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -5,14 +5,25 @@
 #
 
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas as pd
 
-# import data
-dataset = pd.read_csv('data/train.csv')
-X = dataset.iloc[:, 2:13].values
-Y = dataset.iloc[:, 1].values
 
-# replace missing data
-from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(strategy= "mean", missing_values = np.nan)
-X = imputer.fit_transform.(X[:, 5])
+""" It may be more effective to fill in the missing age values by the titles in people's names"""
+def data_preprocessing(dataset):
+    # import data
+    # dataset = pd.read_csv('data/train.csv')
+    X = dataset.iloc[:, 2:13].values
+    Y = dataset.iloc[:, 1].values
+
+    # replace missing data
+    from sklearn.impute import SimpleImputer
+    imputer = SimpleImputer(strategy= "mean", missing_values = np.nan)
+    imputer = imputer.fit(X[:,3])
+   
+    #X = imputer.fit_transform(X[:, 5]) Testing out new code
+    X[:,3] = imputer.transform(X[:,3])
+
+
+# Random Forest Algorithm 
+
